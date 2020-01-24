c23df7d2d710019d9aa8e29d98a43fa40ed214d7
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2020-01-24 09:17:48 -0600

    Connecting django and react

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = props => {
+  return (
+    <div>
+      <h1>Add Lead Form</h1>
+    </div>
+  );
+};
+
+export default Form;
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Leads = props => {
+  return (
+    <div>
+      <h1>Leads List</h1>
+    </div>
+  );
+};
+
+export default Leads;
